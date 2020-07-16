7d6ab289f6894837d13d85cb5fcccf559c4fbd03
Santiago Guerra <santiago.guerra@almundo.com>
2020-07-16 07:13:25 -0300

    Release 0.0.1

---
+++ build/precache-manifest.2883245074042c2d3c1a3a599009013d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24bf84f78ab31afd2d3dba9c050aa3c6",
+    "url": "/pixelame-web/index.html"
+  },
+  {
+    "revision": "469abe03c15acaac0023",
+    "url": "/pixelame-web/static/css/main.15110e50.chunk.css"
+  },
+  {
+    "revision": "399beddd37923095b0d1",
+    "url": "/pixelame-web/static/js/2.08c6eb30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixelame-web/static/js/2.08c6eb30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469abe03c15acaac0023",
+    "url": "/pixelame-web/static/js/main.c4b44bca.chunk.js"
+  },
+  {
+    "revision": "2f852644003a024a4330",
+    "url": "/pixelame-web/static/js/runtime-main.3b9aa378.js"
+  },
+  {
+    "revision": "653424445b5f7f8ef46c93907a00230b",
+    "url": "/pixelame-web/static/media/britta.65342444.png"
+  }
+]);
